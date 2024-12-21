fe12deed9e443f68af0cc6dafee5d5edd17aaaaa
APaud3l <aamod.paudel@coderacademy.edu.au>
2024-12-21 12:06:57 +1100

    feat: Test of get all users route with specific array value expected with a minimum array length

--- tests/users.test.js
+++ tests/users.test.js
@@ -6,10 +6,21 @@ const { app } = require("../src/server.js");
 const request = require("supertest");
 
 describe("Users route", () => {
-    test.skip("'Get all users' route returns array of users", async () => {
+    test("'Get all users' route returns array of users", async () => {
         // GET localhost:3300/users
         const response = await request(app).get("/users");
+
+        const expectedUsers = [
+            "Osman",
+            "Luke",
+            "Venita"
+        ];
+
+        expect(response.body.data).toHaveLength(5);
+        expect(response.body.data.length).toBeGreaterThan(3);
+        expect(response.body.data).toEqual(expect.arrayContaining(expectedUsers));
     });
+
     test.skip("'Get user by ID' route returns a specific user only", async () => {
         // GET localhost:3300/users/:id
         let targetUserId = "1";
@@ -18,39 +29,39 @@ describe("Users route", () => {
     test.skip("'Create a new user' route returns the newly created user", async () => {
         // POST localhost:3300/users/signup
         const response = await request(app)
-                        .post("/users/signup")
-                        .send({
-                            username: "Alice",
-                            password: "abc123"
-                        });
+            .post("/users/signup")
+            .send({
+                username: "Alice",
+                password: "abc123"
+            });
     });
     test.skip("'Login user' route returns a specific user only", async () => {
         // POST localhost:3300/users/login
         const response = await request(app)
-                        .post("/users/login")
-                        .send({
-                            username: "Alice",
-                            password: "abc123"
-                        });
+            .post("/users/login")
+            .send({
+                username: "Alice",
+                password: "abc123"
+            });
     });
     test.skip("'Update/Edit user' route returns a specific user only", async () => {
         // PUT/PATCH localhost:3300/users/:id
         let targetUserId = "1";
         const response = await request(app)
-                        .patch("/users/" + targetUserId)
-                        .send({
-                            username: "Alice",
-                            password: "Abc12345"
-                        });
+            .patch("/users/" + targetUserId)
+            .send({
+                username: "Alice",
+                password: "Abc12345"
+            });
     });
     test.skip("'Delete user by ID' route returns an acknowledgement message", async () => {
         // DELETE localhost:3300/users/:id
         let targetUserId = "1";
         const response = await request(app)
-                        .delete("/users/" + targetUserId)
-                        .send({
-                            username: "Alice",
-                            password: "Abc12345"
-                        });
+            .delete("/users/" + targetUserId)
+            .send({
+                username: "Alice",
+                password: "Abc12345"
+            });
     });
 });
